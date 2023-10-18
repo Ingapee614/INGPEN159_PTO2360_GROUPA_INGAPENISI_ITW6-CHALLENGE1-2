5f79e353d9643e038af1ed83f4c39fa1a6a156fc
Ingapee614 <132918785+Ingapee614@users.noreply.github.com>
2023-10-18 19:35:39 +0200

    isNaN used

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const primaryPhone = 'O748105141'
-const secondaryPhone = '0219131568'
+
+const primaryPhone = 'O748105141';
+const secondaryPhone = '0219131568';
 
 // Only change below this line
-const primaryValid = typeof primaryPhone 
-const secondaryValid = typeof secondaryPhone
-if( primaryValid==="string"&& secondaryValid==="string"){
-console.log('Primary phone is valid numerical string:',Boolean(primaryValid))
-console.log('Secondary phone is valid numerical string:',Boolean(secondaryValid) )   
-}
-else{
-    console.log("primary phone and secondary phone are not valid numerical strings")
-}
+const primaryValid = !isNaN(Number(primaryPhone));
+const secondaryValid = !isNaN(Number(secondaryPhone));
 
+if (primaryValid || secondaryValid) {
+    console.log('Primary phone is a valid numerical string:', primaryValid);
+    console.log('Secondary phone is a valid numerical string:', secondaryValid);
+} else {
+    console.log("Primary phone and/or secondary phone are not valid numerical strings");
+}
 
 
 
